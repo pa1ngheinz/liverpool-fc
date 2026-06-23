256d9cbc4724651eeb0cd25887278fbd164832b3
PaingHeinKyaw <paingheink003@gmail.com>
2026-06-23 17:26:27 +0630

    modified code logic

--- js/trophy.js
+++ js/trophy.js
@@ -11,50 +11,30 @@ async function fetchData() {
     const data = await response.json();
 
     // If json data was empty
-    if (data.length === 0 || data.error === true) {
+    if (data.length === 0) {
       throw new Error("No trophy to show!");
     }
 
     let html = "";
-    data.map((data) => {
+    data.forEach((element) => {
       html += `
             <div class="trophy-card">
             <div class="trophy-thumbnail">
                 <img
-                id="trophy-image"
-                src="${data.src}"
+                class="trophy-image"
+                src="${element.src}"
                 alt="#"
                 />
             </div>
 
             <div class="trophy-info">
-                <p id="trophy-number">${data.trophy}</p>
-                <p id="trophy-name">${data.name}</p>
+                <p class="trophy-number">${element.trophy}</p>
+                <p class="trophy-name">${element.name}</p>
             </div>
             </div>
         `;
     });
     trophyContainer.innerHTML = html;
-
-    // data.forEach((element) => {
-    //   html += `
-    //         <div class="trophy-card">
-    //         <div class="trophy-thumbnail">
-    //             <img
-    //             id="trophy-image"
-    //             src="${element.src}"
-    //             alt="#"
-    //             />
-    //         </div>
-
-    //         <div class="trophy-info">
-    //             <p id="trophy-number">${element.trophy}</p>
-    //             <p id="trophy-name">${element.name}</p>
-    //         </div>
-    //         </div>
-    //     `;
-    // });
-    // trophyContainer.innerHTML = html;
   } catch (error) {
     trophyContainer.classList.add("errMsg");
     let errorMsg = `<div style="color: red; font-size: 2rem">${error.message}</div>`;
