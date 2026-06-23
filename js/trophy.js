096622a5f834c5b810a168f5b075fc8e2017579a
PaingHeinKyaw <paingheink003@gmail.com>
2026-06-23 17:11:04 +0630

    added error msg when fetching went wrong and improve code logic

--- js/trophy.js
+++ js/trophy.js
@@ -1,29 +1,66 @@
 const trophyContainer = document.querySelector(".trophy-container");
-let html = "";
 
 async function fetchData() {
-  const response = await fetch("./data/images.json");
-  const data = await response.json();
+  try {
+    const response = await fetch("./data/images.json");
 
-  data.forEach((element) => {
-    html += `
-        <div class="trophy-card">
-          <div class="trophy-thumbnail">
-            <img
-              id="trophy-image"
-              src="${element.src}"
-              alt="Champion League"
-            />
-          </div>
+    // If fetching wasn't success
+    if (!response.ok) {
+      throw new Error("No trophy to show!");
+    }
+    const data = await response.json();
 
-          <div class="trophy-info">
-            <p id="trophy-number">${element.trophy}</p>
-            <p id="trophy-name">${element.name}</p>
-          </div>
-        </div>
-    `;
+    // If json data was empty
+    if (data.length === 0 || data.error === true) {
+      throw new Error("No trophy to show!");
+    }
+
+    let html = "";
+    data.map((data) => {
+      html += `
+            <div class="trophy-card">
+            <div class="trophy-thumbnail">
+                <img
+                id="trophy-image"
+                src="${data.src}"
+                alt="#"
+                />
+            </div>
+
+            <div class="trophy-info">
+                <p id="trophy-number">${data.trophy}</p>
+                <p id="trophy-name">${data.name}</p>
+            </div>
+            </div>
+        `;
+    });
     trophyContainer.innerHTML = html;
-  });
+
+    // data.forEach((element) => {
+    //   html += `
+    //         <div class="trophy-card">
+    //         <div class="trophy-thumbnail">
+    //             <img
+    //             id="trophy-image"
+    //             src="${element.src}"
+    //             alt="#"
+    //             />
+    //         </div>
+
+    //         <div class="trophy-info">
+    //             <p id="trophy-number">${element.trophy}</p>
+    //             <p id="trophy-name">${element.name}</p>
+    //         </div>
+    //         </div>
+    //     `;
+    // });
+    // trophyContainer.innerHTML = html;
+  } catch (error) {
+    trophyContainer.classList.add("errMsg");
+    let errorMsg = `<div style="color: red; font-size: 2rem">${error.message}</div>`;
+
+    trophyContainer.innerHTML = errorMsg;
+  }
 }
 
 fetchData();
