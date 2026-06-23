a2874c2057d9837bd47b7099010d307dc3d65c6b
PaingHeinKyaw <paingheink003@gmail.com>
2026-06-23 21:03:40 +0630

    added fade-in animation effect for trophy room

--- js/trophy.js
+++ js/trophy.js
@@ -1,5 +1,6 @@
 const trophyContainer = document.querySelector(".trophy-container");
 
+// Fetching data from json
 async function fetchData() {
   try {
     const response = await fetch("./data/images.json");
@@ -18,23 +19,41 @@ async function fetchData() {
     let html = "";
     data.forEach((element) => {
       html += `
-            <div class="trophy-card">
+            <div class="trophy-card fade-in">
             <div class="trophy-thumbnail">
-                <img
-                class="trophy-image"
-                src="${element.src}"
-                alt="#"
-                />
+            <img
+            class="trophy-image"
+            src="${element.src}"
+            alt="#"
+            />
             </div>
-
+            
             <div class="trophy-info">
-                <p class="trophy-number">${element.trophy}</p>
-                <p class="trophy-name">${element.name}</p>
+            <p class="trophy-number">${element.trophy}</p>
+            <p class="trophy-name">${element.name}</p>
             </div>
             </div>
-        `;
+            `;
     });
     trophyContainer.innerHTML = html;
+
+    // FadeIn animation
+    const fadeElements = document.querySelectorAll(".fade-in");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }else{
+            entry.target.classList.remove("show");
+        }
+      })
+    });
+
+    // Main section to observe elements
+    fadeElements.forEach((element) => {
+      observer.observe(element);
+    });
   } catch (error) {
     trophyContainer.classList.add("errMsg");
     let errorMsg = `<div style="color: red; font-size: 2rem">${error.message}</div>`;
