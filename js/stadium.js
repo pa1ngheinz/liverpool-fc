422c88a065eac828875a10b7e62b9b9502e031be
PaingHeinKyaw <paingheink003@gmail.com>
2026-06-24 20:25:34 +0630

    finished image changing function for stadium section

--- js/stadium.js
+++ js/stadium.js
@@ -1,1 +1,12 @@
 const mainImage = document.querySelector("#mainImage");
+const buttons = document.querySelectorAll(".stadium-button");
+
+function imageChange() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      mainImage.src = button.dataset.image;
+    });
+  });
+}
+
+imageChange();
